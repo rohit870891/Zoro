ce51138ca2eca4fdd8fcfaf09fe2a226e06e281b
rohit870891 <koyeb77user@gmail.com>
2024-10-07 00:43:57 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -186,9 +186,11 @@ async def start_command(client: Client, message: Message):
             snt_msgs = []
             for msg in messages:
                 if bool(CUSTOM_CAPTION) & bool(msg.document):
-                    caption = CUSTOM_CAPTION.format(previouscaption="" if not msg.caption else msg.caption.html, filename=msg.document.file_name)
-                else:
-                    caption = "" if not msg.caption else msg.caption.html
+                    original_caption = msg.caption.html if msg.caption else ""
+                    if CUSTOM_CAPTION:
+                        caption = f"{original_caption}\n\n{CUSTOM_CAPTION}"
+                    else:
+                        caption = original_caption
 
                 if DISABLE_CHANNEL_BUTTON:
                     reply_markup = msg.reply_markup
